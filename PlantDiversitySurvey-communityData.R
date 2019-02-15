e3065ca72847850f444e090297fec12904a01d5a
brymz <z.t.brym@gmail.com>
2019-02-15 15:17:33 -0500

    -communtyData.R: add nMDS 100 and 10 subset

--- PlantDiversitySurvey-communityData.R
+++ PlantDiversitySurvey-communityData.R
@@ -6,7 +6,7 @@ library(tidyverse)
 
 build_community_data <- function(species, plots, survey){
   community_data <- matrix(nrow = nrow(plots), ncol = nrow(species))
-  colnames(community_data) <- all_species$genus_species
+  colnames(community_data) <- species$genus_species
   for (c in 1:nrow(species)){
     target_species <- species$genus_species[c]
     for (r in 1:nrow(plots)){
@@ -34,6 +34,7 @@ all_species <- surveys_w_plots %>%
   arrange(genus_species)
   
 tens <- surveys_w_plots %>%
+  filter(!is.na(genus)) %>%
   filter(!is.na(corner)) %>%
   group_by(block, site, big_plot, corner) %>%
   distinct(genus_species)
@@ -42,7 +43,9 @@ status <- data.frame(block = c(1,4,14,15), status = c("old", "old", "new", "new"
 plots_tens <- distinct(tens, big_plot, corner) %>%
   mutate(site_code = paste(block, site, sep = "")) %>%
   left_join(status, by = c("block"))
-plots_hundreds <- distinct(surveys_w_plots, block, site, big_plot)
+plots_hundreds <- distinct(surveys_w_plots, block, site, big_plot) %>%
+  mutate(site_code = paste(block, site, sep = "")) %>%
+  left_join(status, by = c("block"))
 plots_site <- distinct(surveys_w_plots, block, site)
 
 
@@ -70,18 +73,22 @@ ggplot(diversity_ten_tbl, aes(x=diversity, fill=site)) +
 
 ## Non-metric Multidimensional Analysis [PCOA / metaMDS with cmdscale(), vegdist()]
 
+### Tens
+
 #matrix_ten <- read.csv("data/matrix-ten.csv", row =1, header =T) 
 dist_plots <- vegdist(matrix_ten, "bray")
-dist_plots_j <- vegdist(matrix_ten, "jaccard")
-nmds_plots <- metaMDS(dist_plots, k=2, trace=TRUE)
+#dist_plots_j <- vegdist(matrix_ten, "jaccard")
+nmds_plots <- metaMDS(dist_plots, k=2, try=100, trace=TRUE)
 #stressplot(nmds_plots)
 #ordiplot(nmds_plots, display="sites", cex=1.25)
 
 nmds_plots_scores <- plots_tens %>%
   bind_cols(NMDS1 = scores(nmds_plots)[,1], NMDS2 = scores(nmds_plots)[,2])
 
-ggplot(nmds_plots_scores, aes(x=NMDS1, y=NMDS2, shape=site, color=as.factor(block))) +
+ggplot(nmds_plots_scores, aes(x=NMDS1, y=NMDS2, shape=site, color=as.factor(block),
+                            label = paste(big_plot,corner))) +
   geom_point(cex=5) +
+  geom_text(color="black")+
   labs(x="NMDS1", y="NMDS2", shape="Site", color="Block") +
   theme_bw(base_size=20, base_family="Helvetica")
 ggsave("output/nmds.png", width = 8, height = 6)
@@ -95,3 +102,59 @@ sink("output/permanova.txt")
 print(perm_plots)
 sink()
 
+### Tens Subset
+
+species_rank <- tens %>%
+  group_by(genus_species) %>%
+  summarize(count=n()) %>%
+  arrange(desc(count))
+
+top_species <- filter(species_rank, count >5)
+
+matrix_ten <- build_community_data(top_species, plots_tens, tens)
+dist_plots <- vegdist(matrix_ten, "bray")
+nmds_plots <- metaMDS(dist_plots, k=2, try=100, trace=TRUE)
+
+nmds_plots_scores <- plots_tens %>%
+  bind_cols(NMDS1 = scores(nmds_plots)[,1], NMDS2 = scores(nmds_plots)[,2])
+
+ggplot(nmds_plots_scores, aes(x=NMDS1, y=NMDS2, shape=site, color=as.factor(block),
+                              label = paste(big_plot,corner))) +
+  geom_point(cex=5) +
+  geom_text(color="black")+
+  labs(x="NMDS1", y="NMDS2", shape="Site", color="Block") +
+  theme_bw(base_size=20, base_family="Helvetica")
+
+
+perm_plots <- adonis(dist_plots ~ plots_tens$status + plots_tens$block +
+                       plots_tens$site_code, permutations = 1000)
+hist(perm_plots$f.perms)
+anosim_plots <- anosim(dist_plots, plots_tens$block, permutations = 1000)
+
+sink("output/permanova-10s.txt")
+print(perm_plots)
+sink()
+
+### Hundreds
+
+dist_plots <- vegdist(matrix_hundred, "bray")
+nmds_plots <- metaMDS(dist_plots, k=2, try=100, trace=TRUE)
+
+nmds_plots_scores <- plots_hundreds %>%
+  bind_cols(NMDS1 = scores(nmds_plots)[,1], NMDS2 = scores(nmds_plots)[,2])
+
+ggplot(nmds_plots_scores, aes(x=NMDS1, y=NMDS2, shape=site, color=as.factor(block),
+                              label = big_plot)) +
+  geom_point(cex=5) +
+  geom_text(color="black")+
+  labs(x="NMDS1", y="NMDS2", shape="Site", color="Block") +
+  theme_bw(base_size=20, base_family="Helvetica")
+ggsave("output/nmds-100.png", width = 8, height = 6)
+
+perm_plots <- adonis(dist_plots ~ plots_hundreds$status + plots_hundreds$block +
+                       plots_hundreds$site_code, permutations = 1000)
+
+
+sink("output/permanova-100.txt")
+print(perm_plots)
+sink()
