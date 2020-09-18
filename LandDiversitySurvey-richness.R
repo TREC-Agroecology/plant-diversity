3c4d7047508640a25ff108d2ec5acfe693db8330
brymz <z.t.brym@gmail.com>
2020-09-18 16:05:52 -0400

    LandDiversity-richness.R: complete revision

--- LandDiversitySurvey-richness.R
+++ LandDiversitySurvey-richness.R
@@ -1,5 +1,5 @@
 ### This script organizes and evaluates the NEON Plant Diversity Survey
-### conducted at TREC in Homestead, FL Fall 2017 - Spring 2018.
+### conducted at TREC in Homestead, FL Spring 2020 with land history.
 ### Working directory is 'Source File Location'.
 
 
@@ -9,37 +9,22 @@ library(agricolae)
 
 ## Data
 
-surveys <- read_csv("data/PlantDiversitySurvey-surveys.csv")
+surveys <- read_csv("data/NEONsurveyMar2020PlotData.csv")
 status <- read_csv("data/TRECstatus.csv")
 invasive <- read_csv("data/TRECinvasive.csv")
 
 surveys_w_plot <- surveys %>%
-  filter(!is.na(genus)) %>%
-  mutate(genus_species = paste(tolower(str_extract(genus, "...")),
-                               tolower(str_trunc(species, 3, "right", "")), 
-                               sep="")) %>%
-  separate(code, c("big_plot", "corner", "small_plot"), sep="\\.")
+  filter(Certainty == "ToSpp") %>%
+  separate(Subplot, c("big_plot", "corner", "small_plot"), sep="\\.")
      # Expect missing pieces for 100m2 plots
 surveys_w_plots <- surveys_w_plot %>% 
-  select(block, site, big_plot, corner, small_plot, genus_species)
+  select(block = Block, site = Land, big_plot, corner, small_plot, 
+         genus_species = Code_CGM)
 
-echo <- read.csv("data/ECHO-surveys.csv") %>% 
-  filter(UncertainId %in% c(NA, "Species")) %>%
-  mutate(genus_species = tolower(genus_species)) %>%
-  separate(code, c("big_plot", "corner", "small_plot"), sep="\\.") %>%
-  select(block, site, big_plot, corner, small_plot, genus_species)
+surveys_w_plots$block <- str_replace(surveys_w_plots$block, "B02", "B01")
 
-surveys_w_plots <- bind_rows(surveys_w_plots, echo)
-
-pub_sites <- data.frame(block = c(1, 4, 14, 15, 31, 32), 
-                     pub_site = c("TREC-NW", "TREC-NE", "TREC-SW", "TREC-SE", "ECHO-E", "ECHO-W"))
-
-cluster <- data.frame(block = c(1, 1, 4, 4, 14, 14, 15, 15),
-                      site = rep(c("N", "S"), 4),
-                      status = c("high", "high", "low", "high",
-                                 "high", "low", "low", "low"),
-                      cluster = c("open", "open", "lawn", "open",
-                                  "open", "hammock", "orchard", "orchard"))
+# pub_sites <- data.frame(block = c(1, 4, 14, 15, 31, 32), 
+#                      pub_site = c("TREC-NW", "TREC-NE", "TREC-SW", "TREC-SE", "ECHO-E", "ECHO-W"))
 
 ## Scale Summaries
 
@@ -77,24 +62,22 @@ blocks <- surveys_w_plots %>%
 record_counts <- ones %>% 
   right_join(tens) %>% 
   inner_join(big_plots) %>% 
-  mutate(site_stat = paste(block, site, sep="")) %>% 
-  left_join(cluster)
+  mutate(site_stat = paste(block, site, sep=""))
 
 record_counts_sites <- ones %>% 
   right_join(tens) %>% 
   inner_join(big_plots) %>% 
-  mutate(site_stat = paste(block, site, sep="")) %>% 
-  left_join(pub_sites)
+  mutate(site_stat = paste(block, site, sep=""))
 
 
-## Calculate and visualize richness among scales [[TREC ONLY]]
+## Calculate and visualize richness among scales
 
 ggplot(tens, aes(x=tens, fill=site)) +  # tens visualization
   geom_histogram(binwidth = 5) +
   facet_grid(.~block) +
   labs(x="Richness", y="Count", fill="Site") +
   theme_bw(base_size=24, base_family="Helvetica")
-ggsave("output/richness_ten.png", width = 12, height = 5)  
+ggsave("output/2020_richness_ten.png", width = 12, height = 5)  
 
 site_avg <- record_counts %>%
   group_by(block, site) %>%
@@ -103,48 +86,47 @@ site_avg <- record_counts %>%
             avg_hund = round(mean(hundreds), 0), sd_hund = round(sd(hundreds), 2))
 
 site_avg_plot <- record_counts %>%
-  group_by(status, cluster) %>%
+  group_by(block, site, site_stat) %>%
   summarize("1" = round(mean(ones, na.rm=TRUE), 0),
             "10" = round(mean(tens), 0),
             "100" = round(mean(hundreds), 0)) %>% 
-  filter(!is.na(cluster)) %>% 
   gather(scale, average, "1":"100")
 
-ggplot(site_avg_plot, aes(x=scale, y=average, group=cluster)) +
+ggplot(site_avg_plot, aes(x=scale, y=average, group=site_stat)) +
   geom_line() +
-  geom_point(size=3, alpha = 0.8,  aes(shape=status, color=as.factor(cluster))) +
-  labs(x="Scale [m2]", y="Average Richness", shape="Soil Disturbance", color="Habitat") +
+  geom_point(size=3, alpha = 0.8,  aes(shape=site, color=block)) +
+  labs(x="Scale [m2]", y="Average Richness", shape="Site", color="Block") +
   theme_classic(base_size=14, base_family="Helvetica") +
   scale_colour_manual(values=c("#E69F00", "#56B4E9", "#009E73", "#0072B2"))
-ggsave("output/avg_richness.png", width = 5, height = 4)  
+ggsave("output/2020_avg_richness.png", width = 5, height = 4)  
 
 ## ANOVA and Tukey Post Hoc
 
-sink("output/richness.txt")
+sink("output/2020_richness.txt")
 
-hundreds_aov <- aov(hundreds ~ block + site_stat, data=big_plots)
-cat("hundreds ~ block + site_stat\n") 
+hundreds_aov <- aov(hundreds ~ block + site + block*site, data=big_plots)
+cat("hundreds ~ block + site + block*site\n") 
 print(summary(hundreds_aov))
 cat("\nblock\n")
 HSD.test(hundreds_aov, "block")$groups
 cat("\nsite\n")
-HSD.test(hundreds_aov, "site_stat")$groups
+HSD.test(hundreds_aov, "site")$groups
 
-tens_aov <- aov(tens ~ block + site_stat, data=record_counts)
-cat("\ntens ~ block + site_stat\n") 
+tens_aov <- aov(tens ~ block + site + block*site, data=record_counts)
+cat("\ntens ~ block + site + block*site\n") 
 print(summary(tens_aov))
 cat("\nblock\n")
 HSD.test(tens_aov, "block")$groups
 cat("\nsite\n")
-HSD.test(tens_aov, "site_stat")$groups
+HSD.test(tens_aov, "site")$groups
 
-ones_aov <- aov(ones ~ block + site_stat, data=record_counts)
-cat("\nones ~ block + site_stat\n") 
+ones_aov <- aov(ones ~ block + site + block*site, data=record_counts)
+cat("\nones ~ block + site + block*site\n") 
 summary(ones_aov)
 cat("\nblock\n")
 HSD.test(ones_aov, "block")$groups
 cat("\nsite\n")
-HSD.test(ones_aov, "site_stat")$groups
+HSD.test(ones_aov, "site")$groups
 
 sink()
 
@@ -160,7 +142,7 @@ block_status <- surveys_w_plot %>%
   summarize(richness = n(), natives = sum(native, na.rm=TRUE),
             established = sum(established_FL, na.rm=TRUE) - sum(native, na.rm=TRUE))
 
-## Species Area Curves [[TREC ONLY]]
+## Species Area Curves
 
 richness_results <- record_counts %>% 
   ungroup() %>% 
